343e4f78d66b7139948cc2a9602dac849af8730b
ijhanna04 <130183176+ijhanna04@users.noreply.github.com>
2023-04-13 13:22:30 -0500

    lab9

--- CSC 1300 Labs/Lab9_ijhanna42/lab9.cpp
+++ CSC 1300 Labs/Lab9_ijhanna42/lab9.cpp
@@ -17,51 +17,75 @@ int main(){
     int total;
     double average;
     int highestIndex;
+
     char nameLocation[100];
     //pointer to int
-    int* numRecordingsPtr = nullptr;
+    int *numRecordingsPtr = nullptr;
     //pointer to string
-    string* arrayNameInvestigator = nullptr;
+    string *arrayNamePtr = nullptr;
     //pointer to string
-    string* arrayDate = nullptr;
+    string *arrayDatePtr = nullptr;
 
     //ask user for location they are investigating
-    cout << "Hello! What haunted location are you investigating?";
-    cin >> nameLocation;
+    cout << "Hello! What haunted location are you investigating?\n";
+    getline(cin, nameLocation);
 
     //ask user for the number of investigators at the location
-    cout << "How many investigators will be working at this location?";
+    cout << "\nHow many investigators will be working at this location?\n";
     cin >> numInvestigators;
     //validate the number entered is at least 1 but not greater than 25
     while ((numInvestigators < 1)||(numInvestigators > 25)){
-        cout << "Oops! You must enter 1 through 25.";
+        cout << "Oops! You must enter 1 through 25.\n";
+        cout << "\nHow many investigators will be working at this location?\n";
         cin >> numInvestigators;
     }
 
-    getParanormalData(numInvestigators, numRecordingsPtr, arrayNameInvestigator, arrayDate);
-    getStats(numInvestigators, /*number of recordings each investigator had*/, &total, &average, &highestIndex);
+    numRecordingsPtr = new int[numInvestigators];
 
-    cout << lineOfDashes;
+    arrayNamePtr = new string[numInvestigators];
+
+    arrayDatePtr = new string[numInvestigators];
+
+    getParanormalData(numInvestigators, numRecordingsPtr, arrayNamePtr, arrayDatePtr);
+    getStats(numInvestigators, numRecordingsPtr, &total, &average, &highestIndex);
+
+    cout << endl << lineOfDashes;
     cout << "\nThe total amount of EVPs recorded by all " << numInvestigators << " investigators at " << nameLocation << " is " << total << " recordings.\n";
     cout << "\nThe average amount of EVPs recorded at " << nameLocation << " is " << average << " recordings.\n";
-    cout << "\nThe investigator who recorded the most (" << /*most recordings*/ << " recordings) is " << highestIndex << " on " << /*date of most recordings*/"\n";
+    cout << "\nThe investigator who recorded the most (" << numRecordingsPtr[highestIndex] << " recordings) is " << arrayNamePtr[highestIndex] << " on " << arrayDatePtr[highestIndex] << "!!\n";
     cout << lineOfDashes;
-//remember to delete pointer notation
+
+    delete [] numRecordingsPtr;
+    delete [] arrayNamePtr;
+    delete [] arrayDatePtr;
+
 }
 
-void getParanormalData(int numInvestigators, int* numRecordings, string* arrayNames, string* arrayDates){
-    cout << "Enter the investigator's name, the day of their investigation, and the number of EVPs they recorded on their voice recorder.";
+void getParanormalData(int numInvestigators, int *numRecordings, string *arrayNames, string *arrayDates){
+    cout << "\nEnter the investigator's name, the day of their investigation, and the number of EVPs they recorded on their voice recorder.\n";
     for (int i = 0; i < numInvestigators; i++){
         cout << "INVESTIGATOR " << i + 1 << ":\n";
-        cout << right << "NAME - ";
+        cout << "       NAME - ";
         cin >> arrayNames[i];
-        cout << "DAY (ex: 04-03-2023) - ";
+        cout << "       DAY (ex: 04-03-2023) - ";
         cin >> arrayDates[i];
-        cout << "NUMBER OF EVPs - ";
+        cout << "       NUMBER OF EVPs - ";
         cin >> numRecordings[i];
     }
 }
 
-void getStats(int numInvestigators, int* numEVPs, int* totalPtr, double* averagePtr, int* highestIndexPtr){
+void getStats(int numInvestigators, int *numEVPs, int *totalPtr, double *averagePtr, int *indexPtr){
+    int max = 0;
+    *totalPtr = 0;
+    *averagePtr = 0.0;
+    for (int i = 0; i < numInvestigators; i++){
+        *totalPtr += numEVPs[i];
+        *averagePtr = *totalPtr / numInvestigators;
+        if (numEVPs[i] > max){
+            max = numEVPs[i];
+            *indexPtr = i;
+        }
+    }
 
+    
 }
