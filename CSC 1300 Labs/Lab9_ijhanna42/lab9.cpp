1a3a1327e28e67f7fb1cbeb6b5d578e41f4074da
ijhanna04 <130183176+ijhanna04@users.noreply.github.com>
2023-04-13 13:34:35 -0500

    comments for lab9

--- CSC 1300 Labs/Lab9_ijhanna42/lab9.cpp
+++ CSC 1300 Labs/Lab9_ijhanna42/lab9.cpp
@@ -8,21 +8,25 @@
 #include <iostream>
 using namespace std;
 
+//function prototypes
 void getParanormalData(int, int*, string*, string*);
 void getStats(int, int*, int*, double*, int*);
 
 int main(){
+    //variable definintions
     string lineOfDashes(50, '-');
     int numInvestigators;
     int total;
     double average;
     int highestIndex;
-
     char nameLocation[100];
+
     //pointer to int
     int *numRecordingsPtr = nullptr;
+
     //pointer to string
     string *arrayNamePtr = nullptr;
+
     //pointer to string
     string *arrayDatePtr = nullptr;
 
@@ -33,6 +37,7 @@ int main(){
     //ask user for the number of investigators at the location
     cout << "\nHow many investigators will be working at this location?\n";
     cin >> numInvestigators;
+
     //validate the number entered is at least 1 but not greater than 25
     while ((numInvestigators < 1)||(numInvestigators > 25)){
         cout << "Oops! You must enter 1 through 25.\n";
@@ -40,29 +45,33 @@ int main(){
         cin >> numInvestigators;
     }
 
+    //dynamically allocate pointer arrays
     numRecordingsPtr = new int[numInvestigators];
-
     arrayNamePtr = new string[numInvestigators];
-
     arrayDatePtr = new string[numInvestigators];
 
+    //call functions
     getParanormalData(numInvestigators, numRecordingsPtr, arrayNamePtr, arrayDatePtr);
     getStats(numInvestigators, numRecordingsPtr, &total, &average, &highestIndex);
 
+    //print out the results
     cout << endl << lineOfDashes;
     cout << "\nThe total amount of EVPs recorded by all " << numInvestigators << " investigators at " << nameLocation << " is " << total << " recordings.\n";
     cout << "\nThe average amount of EVPs recorded at " << nameLocation << " is " << average << " recordings.\n";
     cout << "\nThe investigator who recorded the most (" << numRecordingsPtr[highestIndex] << " recordings) is " << arrayNamePtr[highestIndex] << " on " << arrayDatePtr[highestIndex] << "!!\n";
     cout << lineOfDashes;
 
+    //release the dynamically allocated arrays
     delete [] numRecordingsPtr;
     delete [] arrayNamePtr;
     delete [] arrayDatePtr;
-
 }
 
+//getParanormalData function
 void getParanormalData(int numInvestigators, int *numRecordings, string *arrayNames, string *arrayDates){
     cout << "\nEnter the investigator's name, the day of their investigation, and the number of EVPs they recorded on their voice recorder.\n";
+    
+    //iterate through each element of the array and allow user to enter a name, date, and number of recordings
     for (int i = 0; i < numInvestigators; i++){
         cout << "INVESTIGATOR " << i + 1 << ":\n";
         cout << "       NAME - ";
@@ -74,13 +83,20 @@ void getParanormalData(int numInvestigators, int *numRecordings, string *arrayNa
     }
 }
 
+//getStats function
 void getStats(int numInvestigators, int *numEVPs, int *totalPtr, double *averagePtr, int *indexPtr){
     int max = 0;
     *totalPtr = 0;
     *averagePtr = 0.0;
+
     for (int i = 0; i < numInvestigators; i++){
+        //find total number of EVPs
         *totalPtr += numEVPs[i];
+
+        //find average number of EVPs
         *averagePtr = *totalPtr / numInvestigators;
+
+        //find investigator with highest number of recordings
         if (numEVPs[i] > max){
             max = numEVPs[i];
             *indexPtr = i;
