2b98e9f4e26b131d51044dad707cd0c6d89d6423
ijhanna04 <130183176+ijhanna04@users.noreply.github.com>
2023-11-07 12:26:18 -0600

    Update BST.h

--- Sophomore/CSC 1310 LAB/Lab8_ijhanna42/BST.h
+++ Sophomore/CSC 1310 LAB/Lab8_ijhanna42/BST.h
@@ -15,6 +15,7 @@ class BST
 		};
 		treeNode* root;
 
+		// copy function
 		void copy(treeNode*& newNode, treeNode* node){
 			if (node == nullptr){
 				newNode = nullptr;
@@ -25,6 +26,7 @@ class BST
 			}
 		}
 
+		// insert function
 		void insert(treeNode*& node, treeNode* newNode){
 			if (node == nullptr){
 				node = newNode;
@@ -35,7 +37,7 @@ class BST
 			}
 		}
 
-
+		// displayInOrder function
 		void displayInOrder(treeNode* node) const{
 			if (node != nullptr){
 				displayInOrder(node->left);
@@ -45,6 +47,7 @@ class BST
 			
 		}
 
+		// destroySubTree function
 		void destroySubTree(treeNode* node){
 			if (node != nullptr){
 				destroySubTree(node->left);
@@ -64,12 +67,14 @@ class BST
 			destroySubTree(root);
 		}
 
-
+		// insertNode function
 		void insertNode(int value){
-			treeNode* newNode = new treeNode(value);
+			treeNode* newNode = new treeNode;
+			newNode->value = value;
 			insert(root, newNode);
 		}
 
+		// display function
 		void display(){
 			displayInOrder(root);
 		}
