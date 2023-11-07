b6b184ccf6a6f498e0de903e961cf9621750f80e
ijhanna04 <130183176+ijhanna04@users.noreply.github.com>
2023-11-07 10:37:27 -0600

    Update BST.h

--- Sophomore/CSC 1310 LAB/Lab8_ijhanna42/BST.h
+++ Sophomore/CSC 1310 LAB/Lab8_ijhanna42/BST.h
@@ -15,18 +15,64 @@ class BST
 		};
 		treeNode* root;
 
-		void copy(treeNode*&, treeNode*);
-		void insert(treeNode*&, treeNode*);
-		void displayInOrder(treeNode*) const;
-		void destroySubTree(treeNode*);
+		void copy(treeNode*& newNode, treeNode* node){
+			if (node == nullptr){
+				newNode = nullptr;
+			} else {
+				newNode->value = node->value;
+				copy(newNode->left, node->right);
+				copy(newNode->right, node->right);
+			}
+		}
+
+		void insert(treeNode*& node, treeNode* newNode){
+			if (node == nullptr){
+				node = newNode;
+			} else if (newNode->value < node->value){
+				insert(node->left, newNode);
+			} else {
+				insert(node->right, newNode);
+			}
+		}
+
+
+		void displayInOrder(treeNode* node) const{
+			if (node != nullptr){
+				displayInOrder(node->left);
+				cout << node->value << " ";
+				displayInOrder(node->right);
+			}
+			
+		}
+
+		void destroySubTree(treeNode* node){
+			if (node != nullptr){
+				destroySubTree(node->left);
+				destroySubTree(node->right);
+				delete node;
+			}
+		}
 
 	public:
-		BST()			//default constructor
-		BST(const BST &tree)	//copy constructor
-		~BST()
+		BST(){			//default constructor
+			root = nullptr;
+		}
+		BST(const BST &tree){	//copy constructor
+			copy(root, tree.root);
+		}
+		~BST(){
+			destroySubTree(root);
+		}
 
-		void insertNode(int);
-		void display()
+
+		void insertNode(int value){
+			treeNode* newNode = new treeNode(value);
+			insert(root, newNode);
+		}
+
+		void display(){
+			displayInOrder(root);
+		}
 };
 
 #endif
