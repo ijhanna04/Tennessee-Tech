2998efcbe430f0717d9edfa3ee2679dcbfd96c54
ijhanna04 <130183176+ijhanna04@users.noreply.github.com>
2023-11-12 11:58:22 -0600

    lab 9

---
+++ Sophomore/CSC 1310 LAB/Lab9_ijhanna42/windows/Driver.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "GraphList.h"
+using namespace std;
+
+int main() {
+
+    // open graph.txt
+    ifstream file("graph.txt");
+
+    int numVertices, vertex1, vertex2;
+
+    // read the number of vertices
+    file >> numVertices;
+    cout << "There are " << numVertices << " vertices in the graph." << endl;
+    
+    // create adjacency list object based on the number of vertices
+    GraphList graph(numVertices);
+
+    // use a loop to read from the file the edges
+    while (file >> vertex1 >> vertex2) {
+
+        // add the edges to the adjacency list
+        graph.addEdge(vertex1, vertex2);
+
+        // print what edge is being added
+        cout << "Adding an edge from " << vertex1 << " to " << vertex2 << "." << endl;
+    }
+
+    // print the adjacency list
+    graph.printGraph();
+
+    return 0;
+}
---
+++ Sophomore/CSC 1310 LAB/Lab9_ijhanna42/windows/GraphList.h
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std;
+
+struct ListNode {
+    int value;
+    ListNode* next;
+};
+
+class GraphList {
+    private:
+        ListNode ** headArray;
+        int numVertices;
+        int numEdges;
+    public:
+
+        // constructor
+        GraphList(int vertices) {
+            numVertices = vertices;
+            headArray = new ListNode*[numVertices];
+            for (int i = 0; i < numVertices; i++) {
+                headArray[i] = nullptr;
+            }
+        }
+
+        // destructor
+        ~GraphList() {
+            for (int i = 0; i < numVertices; i++) {
+                ListNode* current = headArray[i];
+                while (current != nullptr) {
+                    ListNode* next = current->next;
+                    delete current;
+                    current = next;
+                }
+            }
+            delete[] headArray;
+        }
+
+        // addEdge function
+        void addEdge(int vertex1, int vertex2) {
+            ListNode* newNode = new ListNode;
+            newNode->value = vertex2;
+            newNode->next = headArray[vertex1];
+            headArray[vertex1] = newNode;
+        }
+
+        // printGraph function
+        void printGraph() {
+            cout << "Adjacency List..." << endl;
+            for (int i = 0; i < numVertices; i++) {
+                ListNode* current = headArray[i];
+                cout << i << "--->";
+                while (current != nullptr) {
+                    cout << current->value << "--->";
+                    current = current->next;
+                }
+                cout << "NULL" << endl;
+            }
+        }
+};
